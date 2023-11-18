1605d38eda82fd3382ee0afaa910ed7efe458afa
Trần Văn Phúc <phuc260900@gmail.com>
2023-11-19 00:36:58 +0700

    first commit

---
+++ mono/src/main/java/com/phuc/Lec01Stream.java
@@ -0,0 +1,21 @@
+package com.phuc;
+
+import java.util.stream.Stream;
+
+public class Lec01Stream {
+    public static void main(String[] args) {
+        Stream<Integer> stream = Stream.of(1, 2, 3, 4)
+                .map(i -> {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    return i*2;
+                });
+
+//        System.out.println(stream);
+        stream.forEach(System.out::println);
+
+    }
+}
